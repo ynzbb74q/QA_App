3aeabc1394dbd25bc823c14e192ba78d5f1cf1e4
ynzbb74q <ochiai.takayuki@fujitsu.com>
2019-07-19 13:51:04 +0900

    課題着手中のソースを一時コミット

--- app/src/main/java/jp/techacademy/takayuki/ochiai/qa_app/QuestionDetailActivity.kt
+++ app/src/main/java/jp/techacademy/takayuki/ochiai/qa_app/QuestionDetailActivity.kt
@@ -9,11 +9,7 @@ import android.widget.ListView
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_question_detail.*
 
 import java.util.HashMap
@@ -23,6 +19,9 @@ class QuestionDetailActivity : AppCompatActivity() {
     private lateinit var mQuestion: Question
     private lateinit var mAdapter: QuestionDetailListAdapter
     private lateinit var mAnswerRef: DatabaseReference
+
+    val CLIP_OFF = 0
+    val CLIP_ON = 1
 
     private val mEventListener = object : ChildEventListener {
         override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
@@ -100,5 +99,97 @@ class QuestionDetailActivity : AppCompatActivity() {
             .child(mQuestion.genre.toString())
             .child(mQuestion.questionUid).child(AnswersPATH)
         mAnswerRef.addChildEventListener(mEventListener)
+
+        // お気に入りボタンの表示・非表示
+        displayClipFab(mQuestion)
+
+
+
+
+
+    }
+
+
+    // クリップ一覧取得
+    private fun getClipList(): ArrayList<String> {
+        val clipList = ArrayList<String>()
+
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user == null) {
+            // 空のリストを返却
+            return ArrayList()
+        }
+
+        val dataBaseReference = FirebaseDatabase.getInstance().reference
+
+        val clipRef = dataBaseReference
+            .child(UsersPATH)
+            .child(user.uid)
+            .child(ClipPATH)
+
+        clipRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val map = dataSnapshot.value as Map<String, Map<String, String>>
+                for (item in map.values) {
+                    if (item["status"] == CLIP_ON.toString()) {
+                        clipList.add(item["questionUid"]!!)
+                    }
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+
+        return clipList
+    }
+
+
+    private fun displayClipFab(mQuestion: Question) {
+        // 未ログイン時はお気に入りボタンを表示しない
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user == null) {
+            clipFab.hide()
+            return
+        }
+
+        val a = getClipList()
+
+        // お気に入り済みか判定し、ボタンの画像を設定
+        if (getClipList().contains(mQuestion.questionUid)) {
+            clipFab.setImageResource(R.drawable.clip_on_24dp)
+        } else {
+            clipFab.setImageResource(R.drawable.clip_off_24dp)
+        }
+
+
+        // お気に入りボタンのリスナー設定
+        clipFab.setOnClickListener {
+            // お気に入り済みならお気に入り解除
+            // お気に入りしていなければお気に入り
+
+            // TODO　とりあえずお気に入りできるようにしてみる
+
+            val dataBaseReference = FirebaseDatabase.getInstance().reference
+//            val clipRef = dataBaseReference
+//                .child(ContentsPATH)
+//                .child(mQuestion.genre.toString())
+//                .child(mQuestion.questionUid)
+//                .child(ClipPATH)
+//            val data = HashMap<String, String>()
+//            data["uid"] = user.uid
+            val clipRef = dataBaseReference
+                .child(UsersPATH)
+                .child(user.uid)
+                .child(ClipPATH)
+//                .child(mQuestion.questionUid)
+            val data = HashMap<String, String>()
+            data["questionUid"] = mQuestion.questionUid
+            data["status"] = CLIP_ON.toString()
+            clipRef.push().setValue(data)
+        }
+
+
     }
 }
